3ba0dacbe421afc20c05572ae6d7d4f25628c249
jaidheer <jd8331842095@gmail.com>
2022-02-26 06:25:36 +0530

    Added Serialization Code

--- Binary Tree/Serialize.cpp
+++ Binary Tree/Serialize.cpp
@@ -5,8 +5,53 @@
 
 using namespace std; 
 
+typedef struct TreeNode{
+    int val; 
+    TreeNode* right; 
+    TreeNode* left; 
+
+    TreeNode(){
+        val = 0; 
+        right = NULL; 
+        left = NULL; 
+    }
+    TreeNode(int _val){
+        val = _val; 
+        right = NULL; 
+        left = NULL; 
+    }
+} TreeNode; 
+
+TreeNode* makeTree1(){
+    TreeNode* n1 = new TreeNode(10); 
+    TreeNode* n2 = new TreeNode(20); 
+    TreeNode* n3 = new TreeNode(30); 
+    TreeNode* n4 = new TreeNode(40); 
+    TreeNode* n5 = new TreeNode(50); 
+    TreeNode* n6 = new TreeNode(60);
+
+    n1->left = n2; 
+    n1->right = n3; 
+
+    n2->left = n4; 
+    n2->right = n5; 
+
+    n3->right = n6;  
+
+    return n1; 
+}
+
+void preorder(TreeNode* root){
+    if(root == NULL) return; 
+
+    cout<<(root->val)<<" "; 
+    preorder(root->left); 
+    preorder(root->right); 
+}
+
 int main(){
-    cout<<"Hello"<<endl; 
-    
+    TreeNode* root1 = makeTree1(); 
+    preorder(root1); cout<<endl; 
+
     return 0; 
 }
