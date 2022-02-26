d9e09bd7b25a37ddf532412617a5c5927ad5fce7
jaidheer <jd8331842095@gmail.com>
2022-02-26 06:19:20 +0530

    .

---
+++ Binary Tree/Serialize.cpp
@@ -0,0 +1,12 @@
+// Contruct a string which represents a binary tree given the root node
+// Using Preorder Traversal (Root, Left, Right)
+
+#include<iostream> 
+
+using namespace std; 
+
+int main(){
+    cout<<"Hello"<<endl; 
+    
+    return 0; 
+}
